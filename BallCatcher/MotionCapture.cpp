3ebbfb81330ac2a5698de647636af4995c1d46cf
Popsnurkle3 <bendale333@gmail.com>
2015-06-20 12:28:54 -0400

    Updated Project settings
    updating motion capture as well

--- BallCatcher/MotionCapture.cpp
+++ BallCatcher/MotionCapture.cpp
@@ -1,29 +1,62 @@
-#include "opencv2/opencv.hpp"
-#include <Windows.h>
-
-using namespace cv;
-
-int main(int, char**)
-{
-	VideoCapture cap(0); // open the default camera
-	if (!cap.isOpened())  // check if we succeeded
-		return -1;
-	Mat edges;
-	namedWindow("edges", 1);
-	Mat prevframe;
-	Mat nextframe;
-	for (;;)
-	{
-		cap >> prevframe; // get a new frame from camera
-		cap >> nextframe;
-		cvtColor(prevframe, edges, CV_BGR2GRAY);
-		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, 3);
-		imshow("edges", edges);
-		//imshow("Prev", prevframe);
-		imshow("Next", nextframe);
-		if (waitKey(30) >= 0) break;
-	}
-	// the camera will be deinitialized automatically in VideoCapture destructor
-	return 0;
-}
+//#include <opencv2/opencv.hpp>
+//#include <Windows.h>
+//#include <iostream>
+//#include <ctime>
+//
+//
+//
+//using namespace std;
+//using namespace cv;
+//
+//
+//int main(int, char**)
+//{
+//	VideoCapture cap(0); // open the default camera
+//	if (!cap.isOpened())  // check if we succeeded
+//		return -1;
+//	// set camera parameters
+//	//cap.set(CV_CAP_PROP_FPS, 30);
+//	cap.set(CV_CAP_PROP_FRAME_WIDTH, 640);
+//	cap.set(CV_CAP_PROP_FRAME_HEIGHT, 480);
+//
+//	Mat edges;
+//	namedWindow("edges", 1);
+//	Mat current_frame;
+//	int framecounter = 0;
+//
+//	Point textcenter1(100, 50); // text variables start
+//	Point textcenter2(100, 100);
+//	Point textcenter3(100, 150);
+//	int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
+//	double fontScale = 1;
+//	int thickness = 2; // text variables end
+//	int current_time = 0;
+//	unsigned int start = clock(); // timer variable
+//	for (;;)
+//	{
+//
+//		cap >> current_frame; // get a new frame from camera
+//		//cap.grab(); // method for hardware sychronization
+//	//	cap.retrieve(current_frame);
+//
+//		cvtColor(current_frame, edges, CV_BGR2GRAY);
+//		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
+//		Canny(edges, edges, 0, 30, 3);
+//
+//		//Time and Frame tracker and display -> should be exracted to seperate functions
+//		current_time = 1 + ((clock() - start) / 1000);
+//		framecounter++;
+//		//	putText(edges, "Frame count: " + to_string(framecounter++), textcenter1, fontFace, fontScale,Scalar::all(255), thickness, 5);
+//		putText(current_frame, "Seconds past: " + to_string(cap.get(CV_CAP_PROP_FPS)), textcenter2, fontFace, fontScale, Scalar::all(255), thickness, 5);
+//		putText(current_frame, "FPS: " + to_string(framecounter / current_time), textcenter3, fontFace, fontScale, Scalar::all(255), thickness, 5);
+//	//	cout << to_string(framecounter / current_time) + "\n";
+//
+//		imshow("Webcam", current_frame);
+//		imshow("edges", edges);
+//
+//		if (waitKey(30) >= 0) break;
+//
+//	}
+//	// the camera will be deinitialized automatically in VideoCapture destructor
+//	return 0;
+//}
